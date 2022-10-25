ac9c97c5d8fa343dca25a2cb7bae1de8e22d847f
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2022-10-25 17:24:41 -0300

    Update List and main

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -8,7 +8,6 @@ export function List() {
         <strong>Você ainda não tem tarefas cadastradas</strong>
         <p>Crie tarefas e organize seus itens a fazer</p>
       </div>
-
     </div>
   )
 }
