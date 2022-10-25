e2c7745433143f2b9edf6d3ddcad2107fc39ae8c
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2022-10-25 15:30:25 -0300

    Add input and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './styles/global.css'
+import {PlusCircle} from 'phosphor-react'
 
 import { Header } from './components/header'
 
@@ -12,6 +13,20 @@ export function App() {
       <header>
         <Header />
       </header>
+      <div className='flex justify-center mx-auto mt-[-1.8rem] gap-2 max-w-[736px]'>
+        <input
+          type="text"
+          placeholder='Adicione uma nova tarefa'
+          className='flex-1 p-4 rounded-lg bg-base-gray-500 border border-base-gray-700'
+        />
+        <button
+          type='submit'
+          className='flex items-center gap-2 p-4 rounded-lg text-base-gray-100 bg-product-blue-dark'
+        >
+          Criar
+          <PlusCircle size={20}/>
+        </button>
+      </div>
     </div>
   )
 }
