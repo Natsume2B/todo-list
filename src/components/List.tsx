45037a8ce9ea5c2de239a5fdfa896d620b7e6050
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2022-10-25 17:23:05 -0300

    Update List

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -3,7 +3,7 @@ import Clipboard from '../assets/Clipboard.svg'
 export function List() {
   return (
     <div className=" flex flex-col items-center border-t border-t-base-gray-400">
-      <img className='mt-16' src={Clipboard} alt="" />
+      <img className='mt-16' src={Clipboard} alt="Clipboard" />
       <div className="flex flex-col items-center mt-4">
         <strong>Você ainda não tem tarefas cadastradas</strong>
         <p>Crie tarefas e organize seus itens a fazer</p>
