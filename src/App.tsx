7eb112d9b883f8e2ade72a436919d8279d2a087e
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2022-10-26 15:21:30 -0300

    Add Task component

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import './styles/global.css'
 import { Header } from './components/Header'
 import { NewTask } from './components/NewTask'
 import { EmptyList } from './components/EmptyList'
+import { Task } from './components/Task'
+
 
 import { useState } from 'react'
+
 
 
 export function App() {
@@ -17,11 +20,10 @@ export function App() {
       <div className='max-w-[736px] mx-auto'>
         <NewTask />
         <main>
-          <EmptyList />
+          <Task />
+          <Task />
         </main>
       </div>
-
-
     </div>
   )
 }
